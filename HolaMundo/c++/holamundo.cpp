bc2f91e74f8dda8fca12f7a4669f30e34e665359
GerardoGamboa1 <gerardoagm1902@gmail.com>
2022-06-09 15:00:33 -0300

    tp2 sistemas operativos

---
+++ HolaMundo/c++/holamundo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo en: C++\n";
+    return 0;
+}
---
+++ HolaMundo/ruby/holamundo.rb
@@ -0,0 +1,7 @@
+class HolaMundo
+    def initialize
+        puts "Hola Mundo en: Ruby"
+    end
+end
+
+HolaMundo.new
